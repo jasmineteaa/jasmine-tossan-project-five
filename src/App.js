8386b1b2df520c472b32bd9f7e02924e8d6439c1
jasmine <jasmine.tossan@gmail.com>
2019-05-30 01:37:39 -0400

    added on click add song to playlist function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, {Component} from 'react';
 import './App.css';
 import axios from "axios";
-import Form from "./Form"
+import firebase from './firebase';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import Form from "./Form";
+import Playlist from "./Playlist";
+
+library.add(faPlusCircle)
 
 class App extends Component {
+  // keep track of loading - get music
+  // userInput, userCountry
+  // music - array of results matching user inputs
+  // playlist - to append music to
   constructor() {
     super();
+    this.songTitle = React.createRef();
     this.state = {
-      music: [],
       isLoading: false,
       userInput: '',
-      userCountry: 'US'
+      userCountry: 'US',
+      music: [],
+      songTitle: [],
+      playlist: [],
+      selectedSong: ''
     }
   }
-  handleOptionChange = (changeEvent) => {
-    this.setState({
-      userCountry: changeEvent.target.value
-    })
-  }
+
+  // if there is change in input fields, set the new values in state
   handleChange = (event) => {
     this.setState({
-      userInput: event.target.value
+      [event.target.name]: event.target.value
     })
   }
+
+  // on form submit, loader is true
+  // grab user inputs and make axios call to fetch data
   handleSubmit = (event) => {
     event.preventDefault();
     this.setState({
@@ -32,6 +47,27 @@ class App extends Component {
     const userSearch = this.state.userInput;
     this.getData(userSearch, userCountry);
   }
+  // selectSong empty string
+  // on click - see the title attached to the musicItem
+  // store title in selectSong and push as new item in database
+  
+  addSong = mapIndex => {
+    const oldSongTitle = [...this.state.songTitle];
+    const updatedPlaylist = oldSongTitle.filter((item, filterIndex) => filterIndex == mapIndex);
+    const updatedPlaylistString = updatedPlaylist.toString();
+    this.setState ({
+      selectedSong: updatedPlaylistString
+    })
+    const dbRef = firebase.database().ref();
+    dbRef.push(this.state.selectedSong);
+  }
+  // handleClick = (mapIndex) => {
+  //   console.log(this);
+  // }
+
+  // function to get music based on user query and user location inputs
+  // limit results per request to 10
+  // after get data, set loading to false
   getData = (query, location) => {
     axios({
       url: 'https://itunes.apple.com/search',
@@ -40,69 +76,101 @@ class App extends Component {
       params: {
         term: query,
         country: location,
-        limit: 25,
+        limit: 10,
         media: 'music',
       }
     }).then((res) => {
       const data = res.data.results;
-      console.log(data);
+      const songTitle = data.map((item) => {
+        return item.trackName;
+      });
+      console.log(songTitle);
       this.setState({
         music: data,
         isLoading: false,
+        songTitle: songTitle,
       })
 
     })
   }
-
+  // make reference to firebase database
+  // create new array to store the updated playlist state 
+  // if changes in data, push the new data into updatedState array
+  // push all playlist songs into new array
+  // update playlist state with updatedPlaylist array
   componentDidMount() {
-    // axios request here
+
+    const dbRef = firebase.database().ref();
+    dbRef.on('value', (response) => {
+      const data = response.val();
+      const updatePlaylist =[];
+      for (let item in data) {
+        updatePlaylist.push(data[item]);
+      }
+
+      this.setState({
+        playlist: updatePlaylist
+      })
+    })
 
   }
   render() {
 // delete extra artistId and collectionId keys later if don't need 
     return (
       <div className="App">
+          {/* push props to Form component */}
           <Form 
             userInput = {this.state.userInput}
             userCountry = {this.state.userCountry}
             handleChange = {this.handleChange}
             handleSubmit = {this.handleSubmit}
-            handleOptionChange = {this.handleOptionChange}
           />
-          { this.state.isLoading ? 
+          {/* if loading is true, render loading */}
+          {/* if laoding is false, popular song details into div with unique key */}
+          {this.state.isLoading ? 
+
           <p>Loading...</p> :
           
-            this.state.music.map((item) => {
-              const { 
-                trackName,
-                trackId,
-                trackPrice, 
-                currency,
-                artistName, 
-                artistId, 
-                artistViewUrl, 
-                artworkUrl100: artwork, 
-                collectionName, 
-                collectionId, 
-                collectionViewUrl,
-                primaryGenreName, 
-                previewUrl 
-              } = item;
+          this.state.music.map((item, mapIndex) => {
+            const { 
+              trackName,
+              trackId,
+              trackPrice, 
+              currency,
+              artistName, 
+              artistId, 
+              artistViewUrl, 
+              artworkUrl100: artwork, 
+              collectionName, 
+              collectionId, 
+              collectionViewUrl,
+              primaryGenreName, 
+              previewUrl 
+            } = item;
 
-              return (
-                <div className="musicItem" key={trackId}>
-                  <h1>{trackName}</h1>
-                  <div className="image"><img src={artwork} alt={collectionName + 'artwork'} /></div>
-                  <h2 className="artist" key={artistId}><a href={artistViewUrl}>{artistName}</a></h2>
-                  <div className="price">${trackPrice} {currency}</div>
-                  <p className="collection" key={collectionId}><a href={collectionViewUrl}>{collectionName}</a></p>
-                  <p className="genre">{primaryGenreName}</p>
-                  <a href={previewUrl}>preview this song</a>
+            return (
 
-                </div>
-              )}
-              )
-    }
+              <div className="musicItem" key={trackId}>
+                <h1>{trackName}</h1>
+                <div className="image"><img src={artwork} alt={collectionName + 'artwork'} /></div>
+                <h2 className="artist" key={artistId}><a href={artistViewUrl}>{artistName}</a></h2>
+                <div className="price">${trackPrice} {currency}</div>
+                <p className="collection" key={collectionId}><a href={collectionViewUrl}>{collectionName}</a></p>
+                <p className="genre">{primaryGenreName}</p>
+                <a href={previewUrl}>preview this song</a>
+                <button key={mapIndex} onClick={()=>{this.addSong(mapIndex)}}><FontAwesomeIcon icon="plus-circle" />add to playlist</button>
+              </div>
+            )}
+          )
+        }
+        <h1>Your Curated Playlist</h1>
+        <ul className="playlist">
+          {this.state.playlist.map((item) => {
+            return(
+              <li>{item}</li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
--- src/Form.js
+++ src/Form.js
@@ -1,48 +1,47 @@
 import React from 'react'; 
-// restructure form into simples component 
+// restructure form into simple component 
 const Form = (props) => {
     return (
       <form onSubmit={props.handleSubmit}>
         <label>
+          
           Please select your country:
           
           <div className="radio">
             <input type="radio" 
-            name="country" 
-            id="us" 
-            value="US"
-            checked={props.userCountry === 'US'}
-            onChange={props.handleOptionChange}
-            />
+              name="userCountry" 
+              id="us" 
+              value="US"
+              checked={props.userCountry === 'US'}
+              onChange={props.handleChange}/>
             <label htmlFor="us">US</label>
           </div>
 
           <div className="radio">
             <input type="radio" 
-            name="country" 
-            id="canada" 
-            value="CA"
-            onChange={props.handleOptionChange} 
-            checked={props.userCountry === 'CA'}
-            />
+              name="userCountry" 
+              id="canada" 
+              value="CA"
+              onChange={props.handleChange} 
+              checked={props.userCountry === 'CA'}/>
             <label htmlFor="canada">Canada</label>
           </div>
 
         </label> 
-        {/* end of country */}
+        {/* end of country input*/}
 
         <label>
           What are you searching for?
         <input 
-        value={props.userInput}
-        onChange = {props.handleChange}
-        type="text" 
-        placeholder="enter artist, song or music genre" />
+          name="userInput"
+          value={props.userInput}
+          onChange = {props.handleChange}
+          type="text" 
+          placeholder="enter artist, song or music genre" />
         </label>
         {/* end of text input */}
         
-        <input type="submit" 
-        value="Search"/>
+        <input type="submit" value="Search"/>
       </form>
     );
 }
